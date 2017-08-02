a345325e9e12b144b7a9c56163f77ec51f5d5a95
Boris Mikhaylov <kaguxmail@gmail.com>
2017-08-02 09:44:50 +0200

    update to phoenix 1.3

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule ExDebugToolbarDemo.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [{:phoenix, "~> 1.2"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:ecto, github: "elixir-ecto/ecto", branch: "master", override: true},
