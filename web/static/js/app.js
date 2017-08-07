e884924dad0dbd12e7ab9b95623f78c3ae178476
Boris Mikhaylov <kaguxmail@gmail.com>
2017-08-07 21:23:22 +0200

    reproduce issue with jquery

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,10 +1,19 @@
 import $ from "jquery";
-
 window.jQuery = $;
 window.$ = $;
 
-$.fn.test = function() {
- $(this).html('It works!');
+const render = function() {
+  window.$('#test').test();
 }
 
-$('#test').test();
+const test = function() {
+  const $ = window.jQuery;
+  $.fn.test = function() {
+    $(this).html('It works!');
+  }
+
+  console.log("plugin defined");
+  render();
+}
+
+setTimeout(test, 500);
