3f91c14bbbc85d71b73cd0c2a8bb6d5fcb7b422b
Boris Mikhaylov <kaguxmail@gmail.com>
2017-08-08 08:58:20 +0200

    highlight js clash issue better

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,6 +1,7 @@
 import $ from "jquery";
 window.jQuery = $;
 window.$ = $;
+window.jQuery.app = true
 
 const render = function() {
   window.$('#test').test();
@@ -9,11 +10,12 @@ const render = function() {
 const test = function() {
   const $ = window.jQuery;
   $.fn.test = function() {
-    $(this).html('It works!');
+    const msg = $.app? 'It works!' : "Doesn't work, jquery in app is using toolbar's package";
+    $(this).html(msg);
   }
 
   console.log("plugin defined");
   render();
 }
 
-setTimeout(test, 500);
+setTimeout(test, 1000);
